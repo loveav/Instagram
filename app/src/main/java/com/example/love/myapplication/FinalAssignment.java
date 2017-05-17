045bea4ae0a4d1eb4d94e37dfdbeaf5eeec7c9e0
Love <loveav444@gmail.com>
2017-05-17 13:42:50 -0400

    instagram

---
+++ app/src/main/java/com/example/love/myapplication/FinalAssignment.java
@@ -0,0 +1,35 @@
+package com.example.love.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class FinalAssignment extends AppCompatActivity {
+
+    public void mainClick(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+
+    public void mainClick1(View view) {
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+    }
+
+    public void mainClick2(View view) {
+        Intent intent = new Intent(this, Main3Activity.class);
+        startActivity(intent);
+    }
+
+    public void mainClick3(View view) {
+        Intent intent = new Intent(this, Main4Activity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_final_assignment);
+    }
+}
---
+++ app/src/main/java/com/example/love/myapplication/Main2Activity.java
@@ -0,0 +1,22 @@
+package com.example.love.myapplication;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class Main2Activity extends AppCompatActivity {
+
+
+    public void mainClick(View view) {
+        Intent intent = new Intent(this, FinalAssignment.class);
+        startActivity(intent);
+    }
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
